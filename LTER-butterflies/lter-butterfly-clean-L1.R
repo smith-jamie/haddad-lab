68595db66b8f0872175eade7dc81745e90cb0885
Jamie Smith <SMIT3139@msu.edu>
2023-12-22 15:03:17 -0500

    Update to cleaning code

---
+++ LTER-butterflies/lter-butterfly-clean-L1.R
@@ -0,0 +1,10 @@
+
+# AUTHORS:        Nick Haddad
+# COLLABORATORS:  
+# DATA INPUT:     Data imported as csv files from shared REX Google drive Butterfly Surveys L0 folder
+# DATA OUTPUT:    Clean L1 data uploaded to Butterfly Surveys L1 folder
+# PROJECT:        LTER Butterflies
+# DATE:           Dec 2023
+
+# Load packages
+library(dplyr)
