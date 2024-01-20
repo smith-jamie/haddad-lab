fe819afff0e752444e7e9d21e2bf9d3bbbba9c07
Jamie Smith <SMIT3139@msu.edu>
2024-01-20 12:47:53 -0500

    updated cleaning code for L1

--- LTER-butterflies/lter-butterfly-clean-L1.R
+++ LTER-butterflies/lter-butterfly-clean-L1.R
@@ -6,5 +6,20 @@
 # PROJECT:        LTER Butterflies
 # DATE:           Dec 2023
 
-# Load packages
-library(dplyr)
+# set working directory
+setwd("~/Work/MSU Haddad R Directory/haddad-lab")
+
+# read in data
+bfly <- read.csv(file.choose(),header=T)
+
+# check column names
+colnames(bfly)
+
+# check categories
+xtabs(~Transect,data=bfly) # should be strip, walking, NA
+xtabs(~Treatment,data=bfly) # should be CLE, T1-T4, T6, T7
+xtabs(~CLE.diversity,data=bfly) # should be high, low, NA
+
+# data checked and clean
+# export as L1
+write.csv(bfly.w, "LTER-butterflies\\LTER_butterflies_2019-2023_L1.csv", row.names=FALSE)
